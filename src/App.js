d30f5d5b1fbc3cd574aa61f10ae2bfd418769870
IMDAD <ahmedimdad1593@gmail.com>
2020-11-20 11:30:07 +0600

    upto pricing section done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Achievements from "./Components/Achievements/Achievements";
+import Banner from "./Components/Banner/Banner";
+import Banner2 from "./Components/Banner2/Banner2";
+import NavBar from "./Components/NavBar/NavBar";
+import Services from "./Components/Services/Services";
 
 function App() {
   return (
-    <div>
-      
+    <div className="app">
+      <NavBar></NavBar>
+      <Banner></Banner>
+      <Services></Services>
+      <Banner2></Banner2>
+      <Achievements></Achievements>
     </div>
   );
 }
